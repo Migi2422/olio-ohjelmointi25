0e452a3f0a32e439de5988f09f66b64d35e77ace
Migi2422 <milauril24@students.oamk.fi>
2025-01-10 13:56:39 +0200

    valmis tht

--- tht1_25/main.cpp
+++ tht1_25/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
-
 using namespace std;
+int arvauspeli(int maxnum);
 
-int main()
-{
-    cout << "toivottavasti toimii!" << endl;
-    return 0;
+int main(){
+    int ylaraja;
+    cout << "*****Arvauspeli*****" << endl;
+    cout << "laita jokin luku ylarajaksi" << endl;
+    cin >> ylaraja;
+    cout << "arvaa luku 1 - " << ylaraja << endl;
+    arvauspeli(ylaraja);
+
+
+return 0;
+}
+
+
+int arvauspeli(int maxnum){
+
+    int num;
+    int guess;
+    int tries;
+    srand(time(NULL));
+
+    num = rand() % maxnum + 1;
+
+
+
+    do{
+        cin >> guess;
+        tries++;
+
+
+        if(guess > num){
+            cout << "Luku on pienempi" << endl;
+        }
+
+        else if(guess < num){
+            cout << "luku on isompi" << endl;
+        }
+
+        else{
+            cout << "Oikea vastaus # arvausten maara " << tries << endl;
+        }
+
+    }while(guess != num);
+    return tries;
 }
